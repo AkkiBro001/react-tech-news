e3b316abf41b0fc8ad3e6fd1a9dd9de883c28f59
Akshay Tambe <akshay.design4art@gmail.com>
2022-10-06 22:18:44 +0530

    Updates

---
+++ precache-manifest.6ccdb2751baf74cebd9c96656766be9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ececba9fb227ba0cd87c",
+    "url": "/react-tech-news/static/css/main.3044c3f6.chunk.css"
+  },
+  {
+    "revision": "ececba9fb227ba0cd87c",
+    "url": "/react-tech-news/static/js/main.ececba9f.chunk.js"
+  },
+  {
+    "revision": "fcbaed14f5873259352c",
+    "url": "/react-tech-news/static/css/1.04d5bc2b.chunk.css"
+  },
+  {
+    "revision": "fcbaed14f5873259352c",
+    "url": "/react-tech-news/static/js/1.fcbaed14.chunk.js"
+  },
+  {
+    "revision": "db3423ddabc1fbfde4e3",
+    "url": "/react-tech-news/static/js/runtime~main.db3423dd.js"
+  },
+  {
+    "revision": "f92a223d84dd9a9ec210f3dcc55aa8f8",
+    "url": "/react-tech-news/static/media/tech-bg.f92a223d.jpg"
+  },
+  {
+    "revision": "dc36db3f8643659dd0abd3f76bd9fbd4",
+    "url": "/react-tech-news/index.html"
+  }
+];
